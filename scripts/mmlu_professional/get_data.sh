c32096175b576882f9303e2c7a7c3c9c0b796de9
Tu Nguyen <tunguyen1@clariden-ln002.cscs.ch>
2025-11-14 16:55:51 +0100

    Dynamic username

--- scripts/mmlu_professional/get_data.sh
+++ scripts/mmlu_professional/get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-OUT_DIR="/iopsstor/scratch/cscs/tunguyen1/apertus/processed_datasets"
+OUT_DIR="/iopsstor/scratch/cscs/$USER/apertus/processed_datasets"
 
 python3 "$(dirname "$0")/../download_datasets.py" \
     --out_dir "$OUT_DIR" \
